8df3bbe0f4d25d718ae92d2aa8ac9d45dfe49186
kousick <kousick.s@hashedin.com>
2014-07-08 11:23:10 +0530

    Completed With Year and Genre Values

--- Movie.py
+++ Movie.py
@@ -1,14 +1,14 @@
 import operator
-
+import collections
 genresdict={}
 count={}
 avgsum={}
 moviesdict={}
 usersdict={}
 ratingsdict={}
-userwatchcount={} 
-movieswithparticularyear={}
-moviesbyparticulargenre={}
+ 
+
+
 # Creating Movie Dict
 with file("resources/movie.data") as f:
     content = [line.rstrip() for line in f]
@@ -63,19 +63,21 @@ def average_Rating_Calculation():
 #Getting Most Active User
 
 def get_Most_Active_User():
+    userwatchcount={}
     for x,y in ratingsdict.items():
         if y[0] in userwatchcount:
             countsum1=float(userwatchcount.get(y[0])) + 1
             userwatchcount[y[0]]=countsum1
         else:
             userwatchcount[y[0]]=1
-    most_active_user=max(userwatchcount.iteritems(), key=lambda l: l[1])[0]
+    most_active_user=max(userwatchcount.iteritems(), key=operator.itemgetter(1))[0]
     print "Most Active User:",usersdict[most_active_user][0];
     
 #Get Movie Id's for a particular year
 
-def get_Movies_By_Year(year):
+def get_Top_Movie_By_Year(year):
     #average_Rating_Calculation()
+    movieswithparticularyear={}
     with file("resources/movie.data") as f:
         moviecontent = [line.rstrip() for line in f]
         for x in moviecontent:
@@ -86,12 +88,31 @@ def get_Movies_By_Year(year):
                     keyval=y[0]
                     movieswithparticularyear[keyval]=float(avgsum.get(keyval)) 
     #print avgsum
-    highest_rated_movie_by_year=max(movieswithparticularyear.iteritems(), key=lambda lk: lk[1])[0]
-    print highest_rated_movie_by_year
-    print "Highest Average Rated Movie:",moviesdict[highest_rated_movie_by_year][1];
+    #movieswithparticularyear = collections.OrderedDict(sorted(d.items()))
+    highest_rated_movie_by_year=max(movieswithparticularyear.iteritems(), key=operator.itemgetter(1))[0]
+    print "Top Movie By Year:", year + "-" + moviesdict[highest_rated_movie_by_year][1];
     
-def get_Movies_By_Genre(genre):
-    #average_Rating_Calculation()
+def get_Top_Movie_By_Genre(genre):
+    
+    moviesbyparticulargenre={}
+    with file("resources/movie.data") as f:
+        moviecontent = [line.rstrip() for line in f]
+        genreindex=genresdict.get(genre)
+        print genreindex
+        for x in moviecontent:
+            y= x.split('|')
+            value=int(y[5+int(genreindex)])
+            
+            if value==1:
+                
+                moviesbyparticulargenre[y[0]]=float(avgsum[y[0]])
+                
+    highest_rated_movie_by_genre=max(moviesbyparticulargenre.iteritems(),key=operator.itemgetter(1))[0]
+    print "Top Movie By Genre:", genre + "-" + moviesdict[highest_rated_movie_by_genre][1];
+    
+def get_Top_Movie_By_YearAndGenre(year,genre):
+    moviesbyparticulargenre={}
+    movieswithparticularyear={}
     with file("resources/movie.data") as f:
         moviecontent = [line.rstrip() for line in f]
         genreindex=genresdict.get(genre)
@@ -100,23 +121,29 @@ def get_Movies_By_Genre(genre):
             y= x.split('|')
             value=int(y[5+int(genreindex)])
             if value==1:
-                moviesbyparticulargenre[y[0]]=float(avgsum[y[0]])
-    highest_rated_movie_by_genre=max(moviesbyparticulargenre.iteritems(), key=lambda l: l[1])[0]
-    print "Highest Average Rated Movie:",moviesdict[highest_rated_movie_by_genre][1];
+                moviesbyparticulargenre[y[0]]=y[2]
+        for k,l in moviesbyparticulargenre.items():
+            m=l.split('-')
+            if m[0] is not '':
+                if m[2]==year:
+                    keyval=y[0]
+                    movieswithparticularyear[k]=float(avgsum.get(k))      
+    highest_rated_movie_by_yearngenre=max(movieswithparticularyear.iteritems(), key=operator.itemgetter(1))[0]
+    print "Top Movie By Year and Genre:", year + genre + "-" + moviesdict[highest_rated_movie_by_yearngenre][1];            
 
 def get_Highest_Rated_Movie():
     average_Rating_Calculation()
-    highest_rated_movie=max(avgsum.iteritems(), key=lambda l: l[1])[0]
+    highest_rated_movie=max(avgsum.iteritems(), key=operator.itemgetter(1))[0]
     print "Highest Average Rated Movie:",moviesdict[highest_rated_movie][1];
 def get_Most_Watched_Movie():
     average_Rating_Calculation()
-    most_watched_movie=max(count.iteritems(), key=lambda l: l[1])[0]
+    most_watched_movie=max(count.iteritems(), key=operator.itemgetter(1))[0]
     print "Most Watched Movie:",moviesdict[most_watched_movie][1];
     
 get_Highest_Rated_Movie()
 get_Most_Watched_Movie()
 get_Most_Active_User()
-get_Movies_By_Year('1996')
-get_Movies_By_Genre('Animation')
-
+get_Top_Movie_By_Year('1996')
+get_Top_Movie_By_Genre('Animation')
+get_Top_Movie_By_YearAndGenre('1996','Animation')
 #print movieswithparticularyear
